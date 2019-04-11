21b9e522b7e24909a5501291b087c0de00124b60
zemond <3em0nd@gmail.com>
2019-04-11 14:37:46 +0300

    terraform #2

---
+++ terraform/prod/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  # Версия terraform  
+  # required_version = "0.11.11"
+  required_version = ">=0.11,<0.12"
+
+  backend "gcs" {
+    bucket = "storage-bucket-prod-1"
+    prefix = "prod"
+  }
+}
+
+provider "google" {
+  # Версия провайдера  
+  version = "2.0.0"
+
+  # ID проекта  
+  project = "${var.project}"
+
+  region = "${var.region}"
+}
+
+module "app" {
+  source = "../modules/app"
+
+  public_key_path = "${var.public_key_path}"
+
+  zone = "${var.zone}"
+
+  app_disk_image = "${var.app_disk_image}"
+}
+
+module "db" {
+  source = "../modules/db"
+
+  public_key_path = "${var.public_key_path}"
+
+  zone = "${var.zone}"
+
+  db_disk_image = "${var.db_disk_image}"
+}
+
+module "vpc" {
+  source        = "../modules/vpc"
+  source_ranges = ["82.155.222.156/32"]
+}
